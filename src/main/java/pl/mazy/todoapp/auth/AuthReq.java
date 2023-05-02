fef3c5fc2146136f1d7f684b9faa8297be7be9c6
MaZy <maciej.zet004@gmail.com>
2023-05-02 15:29:18 +0200

    many users under one email block

--- src/main/java/pl/mazy/todoapp/auth/AuthReq.java
+++ src/main/java/pl/mazy/todoapp/auth/AuthReq.java
@@ -3,16 +3,22 @@ package pl.mazy.todoapp.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.mazy.todoapp.repository.UserRepo;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthReq {
     private final AuthService service;
+    private final UserRepo uR;
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
-        return ResponseEntity.ok(service.register(request));
+        if (uR.findByEMail(request.getEmail()).isEmpty()) {
+            return ResponseEntity.ok(service.register(request));
+        }else {
+            return (ResponseEntity<AuthResponse>) ResponseEntity.badRequest();
+        }
     }
 
     @PostMapping("/authenticate")
